63ddef1b7d3d61b3d77fa6b4cb500997fbdf901b
abdalla980 <100001276@stud.srh-university.de>
2025-09-02 06:09:08 +0400

    almost

--- src/Footer/footer.tsx
+++ src/Footer/footer.tsx
@@ -2,7 +2,17 @@ import styles from './footer.module.css';
 
 
 export const Footer = () => {
+    const goTosocials = () => {
+        window.open("https://www.instagram.com/futurone_berlin?igsh=MXVjdjdiMGNkMHQ0Mg%3D%3D&utm_source=qr", "_blank");
+    }
+    const GotoKimberlyLinkedin = () => {
+        window.open("https://www.linkedin.com/in/kimberly-mareen-richter-6a1a92369/", "_blank");
+    }
+    const GotoEmails = () => {
+        window.open("futurone.berlin@gmail.com", "_blank");
+    }
     return (
+        <div className={styles.main}>
         <div className={styles.footer}>
             <div className={styles.left}>
                 <h1>Commons of exchange</h1>
@@ -17,16 +27,16 @@ export const Footer = () => {
             </div>
             <div className={styles.right}>
                 <h3>Stay in Contact</h3>
-                <div>
-                    <img className={styles.emailIcon} src={"email.png"}/>
-                    <img src={"instagram.png"}/>
-                    <img src={"linkedin.png"}/>
+                <div className={styles.images}>
+                    <img onClick={GotoEmails} className={styles.emailIcon} src={"email.png"}/>
+                    <img onClick={goTosocials} src={"instagram.png"}/>
+                    <img onClick={GotoKimberlyLinkedin} src={"linkedin.png"}/>
 
                 </div>
 
             </div>
         </div>
-
+        </div>
     )
 
 }
